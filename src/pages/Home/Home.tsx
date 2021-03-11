2009d36c5079a947f5b05c61233e7fcb5d9b50c0
jpveooys <66470099+jpveooys@users.noreply.github.com>
2021-03-11 15:29:27 +0000

    Add repro for timeline day width imprecision problem

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import {
+  Timeline,
+  TimelineDays,
+  TimelineMonths, TimelineRows,
+  TimelineTodayMarker,
+  TimelineWeeks
+} from "@royalnavy/react-component-library";
 
 import { Main } from '../../layouts'
+
+
+const ExampleTimeline = () => (
+  <Timeline
+    startDate={new Date(2020, 9, 5)}
+    endDate={new Date(2021, 1, 1)}
+    unitWidth={53}
+  >
+    <TimelineTodayMarker />
+    <TimelineMonths />
+    <TimelineWeeks />
+    <TimelineDays />
+    <TimelineRows>{}</TimelineRows>
+  </Timeline>
+)
 
 export const Home: React.FC<unknown> = () => (
   <Main>
     <h1>Hello, World!</h1>
+    <ExampleTimeline />
   </Main>
 )
