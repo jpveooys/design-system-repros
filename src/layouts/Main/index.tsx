5d1f6b80376581559791910736dd23db61d90a2b
jpveooys <66470099+jpveooys@users.noreply.github.com>
2021-03-11 15:29:27 +0000

    Add Timeline last day repro

--- src/layouts/Main/index.tsx
+++ src/layouts/Main/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styled from "styled-components";
+
+
+const Wrapper = styled.div`
+  padding: 2rem;
+`
 
 export interface MainProps {
   children?: React.ReactNode
 }
 
-export const Main: React.FC<MainProps> = ({ children }) => <>{children}</>
+export const Main: React.FC<MainProps> = ({ children }) => <Wrapper>{children}</Wrapper>
 
 Main.displayName = 'Main-Layout'
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,66 @@
+import {
+  Timeline,
+  TimelineDays,
+  TimelineEvent,
+  TimelineEvents,
+  TimelineMonths,
+  TimelineRow,
+  TimelineRows,
+  TimelineTodayMarker,
+  useTimelinePosition
+} from "@royalnavy/react-component-library";
 import React from 'react'
 
-import { Main } from '../../layouts'
+import {Main} from '../../layouts'
+
+const CustomComponent = () => {
+  const date1 = new Date(2021, 0, 31)
+  const date2 = new Date(2021, 0, 31, 12)
+
+  const {startsAfterEnd: date1StartsAfterEnd} = useTimelinePosition(date1, date1)
+  const {startsAfterEnd: date2StartsAfterEnd} = useTimelinePosition(date2, date2)
+
+  // eslint-disable-next-line no-console
+  console.log({date1StartsAfterEnd, date2StartsAfterEnd})
+
+  return null
+}
+
 
 export const Home: React.FC<unknown> = () => (
   <Main>
     <h1>Hello, World!</h1>
+    <Timeline
+      startDate={new Date(2021, 0, 1)}
+      range={1}
+      today={new Date(2021, 0, 31, 12)}
+    >
+      <CustomComponent/>
+      <TimelineTodayMarker />
+      <TimelineMonths />
+      <TimelineDays />
+      <TimelineRows>
+        <TimelineRow name="Row 1">
+          <TimelineEvents>
+            <TimelineEvent
+              startDate={new Date(2021, 0, 31, 0, 0, 0)}
+              endDate={new Date(2021, 1, 1, 18, 0, 0)}
+            >
+              Event 1
+            </TimelineEvent>
+          </TimelineEvents>
+        </TimelineRow>
+        <TimelineRow name="Row 1">
+          <TimelineEvents>
+            <TimelineEvent
+              startDate={new Date(2021, 0, 31, 6, 0, 0)}
+              endDate={new Date(2021, 1, 1, 18, 0, 0)}
+            >
+              Event 2
+            </TimelineEvent>
+          </TimelineEvents>
+        </TimelineRow>
+      </TimelineRows>
+    </Timeline>
   </Main>
 )
