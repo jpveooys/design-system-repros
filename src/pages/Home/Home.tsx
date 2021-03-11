c72619e0603eaf8050e1a09bd5a0089506a117e4
jpveooys <66470099+jpveooys@users.noreply.github.com>
2021-03-11 15:29:27 +0000

    Add DatePicker validation repro

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,18 @@
+import { DatePicker } from "@royalnavy/react-component-library";
 import React from 'react'
+import styled from "styled-components";
 
 import { Main } from '../../layouts'
+
+const DatePickerWrapper = styled.div`
+  width: 300px;
+`
 
 export const Home: React.FC<unknown> = () => (
   <Main>
     <h1>Hello, World!</h1>
+    <DatePickerWrapper>
+      <DatePicker />
+    </DatePickerWrapper>
   </Main>
 )
